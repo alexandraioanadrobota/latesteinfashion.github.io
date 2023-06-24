cf80c7297e7276a7b95dc28dec148d50971e68f7
alexandraioanadrobota <133023970+alexandraioanadrobota@users.noreply.github.com>
2023-06-24 23:42:49 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+window.addEventListener ('scroll', function() {
+  var navbar = document.querySelector('.navbar');
+  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  
+  if (scrolled > 0) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
